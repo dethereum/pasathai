d4c7891a025d68313b61ba2c260190701ddcf074
haiturtle <33427880+dethereum@users.noreply.github.com>
2022-09-12 12:49:18 -0400

    ADD: get tone information

--- thaitoroman/app.py
+++ thaitoroman/app.py
@@ -5,19 +5,16 @@ test_data="โรงหนัง"
 
 data = ["มา", "ไก่", "ได้", "นับ", "ฉัน"]
 engines = [
-#    "icu",
-    "ipa",
-    "thaig2p",
     "tltk_ipa",
     "tltk_g2p",
-    "iso_11940"
 ]
 
 
 def my_function():
     for engine in engines:
-        print("Using {}".format(engine))
+        print("Using {}\n".format(engine))
 
         for d in data:
             print(transliterate(d, engine=engine))
 
+        print("\n")
