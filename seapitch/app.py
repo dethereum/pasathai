dee8c9a9de6d7eb274570bd9bc8e0718ff9001af
haiturtle <33427880+dethereum@users.noreply.github.com>
2022-09-24 21:01:12 -0400

    ADD: new route to seapitch for own recordings

--- seapitch/app.py
+++ seapitch/app.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from flask import Flask, request, jsonify
 import parselmouth
 from flask_cors import CORS
@@ -6,6 +8,13 @@ import numpy as np
 
 app = Flask(__name__)
 CORS(app)
+
+
+def get_recording_names(card_id):
+    path = os.environ.get(
+        'HOME') + '/Library/Application Support/Anki2/addons21/1508039970/user_files/recordings/' + card_id
+
+    return [(rec, path + '/' + rec) for rec in os.listdir(path)]
 
 
 def get_pitch_track_from_field(field):
@@ -20,12 +29,35 @@ def get_pitch_track_from_field(field):
     return {'y': list(pitch_values), 'x': list(pitch.xs())}
 
 
-@app.route('/pitch_track', methods=['POST'])
-def pitch_track():
-    import parselmouth
+def get_pitch_track_from_rec(rec):
+    print(rec)
+    sound = parselmouth.Sound(rec)
 
+    pitch = sound.to_pitch()
+    pitch_values = pitch.selected_array['frequency']
+
+    return {'y': list(pitch_values), 'x': list(pitch.xs())}
+
+
+@app.route('/pitch_track_by_field', methods=['POST'])
+def pitch_track_by_field():
     files = json.loads(request.data)['files']
 
     pitch_tracks = [get_pitch_track_from_field(file) for file in files]
 
     return jsonify(pitch_tracks)
+
+
+@app.route('/pitch_tracks', methods=['POST'])
+def pitch_tracks():
+    card_id = json.loads(request.data)['card-id']
+
+    p_tracks = []
+    ids = []
+    for rec_id, rec_path in get_recording_names(card_id):
+        pitch_track = {rec_id: get_pitch_track_from_rec(rec_path)}
+
+        p_tracks.append(pitch_track)
+        ids.append(rec_id)
+
+    return jsonify({'recordings': p_tracks, 'ids': ids})
